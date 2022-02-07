31df8c4affa822e5b0b5192feb0bf88cbde2648c
siavash.ta <siavash.ta@outlook.com>
2022-02-07 18:56:16 +0330

    add exercise 4

--- week3/ex4.js
+++ week3/ex4.js
@@ -21,56 +21,16 @@ you have more than 3 items in your shopping cart the first item gets taken out.
 3. Confirm that your code passes the unit tests.
 -----------------------------------------------------------------------------*/
 const shoppingCart = ['bananas', 'milk'];
+const list=['chocolate','waffles','tea'];
+let i
 
-// ! Function to be tested
-function addToShoppingCart( /* parameters go here */ ) {
-    // TODO complete this function
-}
+  for (i=0;i<list.length;i++) {
+  
+  shoppingCart.push(list[i]);
 
-// ! Test functions (plain vanilla JavaScript)
-function test1() {
-    console.log(
-        'Test 1: addShoppingCart() called without an argument should leave the shopping cart unchanged'
-    );
-    const expected = 'You bought bananas, milk!';
-    const actual = addToShoppingCart();
-    console.assert(actual === expected);
-}
-
-function test2() {
-    console.log('Test 2: addShoppingCart() should take one parameter');
-    const expected = 1;
-    const actual = addToShoppingCart.length;
-    console.assert(actual === expected);
-}
-
-function test3() {
-    console.log('Test 3: `chocolate` should be added');
-    const expected = 'You bought bananas, milk, chocolate!';
-    const actual = addToShoppingCart('chocolate');
-    console.assert(actual === expected);
-}
-
-function test4() {
-    console.log('Test 4: `waffles` should be added and `bananas` removed');
-    const expected = 'You bought milk, chocolate, waffles!';
-    const actual = addToShoppingCart('waffles');
-    console.assert(actual === expected);
-}
-
-function test5() {
-    console.log('Test 5: `tea` should be added and `milk` removed');
-    const expected = 'You bought chocolate, waffles, tea!';
-    const actual = addToShoppingCart('tea');
-    console.assert(actual === expected);
-}
-
-function test() {
-    test1();
-    test2();
-    test3();
-    test4();
-    test5();
-}
-
-test();
+  if (shoppingCart.length>3){
+shoppingCart.shift();
+  };
+  console.log('you bought ',shoppingCart,' !');
+};
+console.log('you bought ',shoppingCart,' !');
